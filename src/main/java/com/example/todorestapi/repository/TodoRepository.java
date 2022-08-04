28e9e8190ce833648ce714af43b729affbad3296
maulanwalker <maulana.hasanudin.tif18@gmail.com>
2022-08-05 01:15:02 +0700

    deploying to heroku

--- src/main/java/com/example/todorestapi/repository/TodoRepository.java
+++ src/main/java/com/example/todorestapi/repository/TodoRepository.java
@@ -1,15 +1,11 @@
 package com.example.todorestapi.repository;
 
-//import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.todorestapi.model.TodoModel;
 
 public interface TodoRepository extends JpaRepository<TodoModel, Long> {
-
 	List<TodoModel> findByTodoTextContaining(String todo_text);
-//	  List<TodoModel> findByTodoByCompleted(boolean todo_is_completed);
 
-//	  List<TodoModel> findByTitleContaining(String todo_text);
 }
